131b779c0ea56d541c8e10f82aa56e785e83ef63
Jo Sangwon <sangwon8895@naver.com>
2022-11-21 11:56:20 +0900

    3

--- 1121/hello.py
+++ 1121/hello.py
@@ -1,4 +1,19 @@
-print(aaa)
+print('hello')
+# 데이터형 /변수
+# 데이터구조(리스트, 딕셔너리, 튜플, 집합)
+# 제어문(조건문, 반복문)
+# 클래스 /함수
+# 외부 라이브러리 사용 (데이터 분석 = pandas)
+# 외부 라이브러리 사용 (통계 = numpy)
+# 시각화 (matplotlib)
 
-print(aaa)
-print(aaa)
+a = 10
+b = 20
+c = '파이썬'
+print(a+b)
+print(type(c))
+
+num1 = int(input('input num1: '))
+num2 = int(input('input num2: '))
+
+print(f"{num1} + {num2} = {num1 + num2}")
